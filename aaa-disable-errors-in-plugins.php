2d56ac06ac546ea702e1654df06ca0e7f8f2730c
Marin Atanasov <tyxla@abv.bg>
2015-01-26 22:01:32 +0200

    improve documentation

--- aaa-disable-errors-in-plugins.php
+++ aaa-disable-errors-in-plugins.php
@@ -21,6 +21,8 @@ class Disable_Errors_In_Plugins {
 	 * Initializes and hooks the plugin functionality.
 	 *
 	 * @access public
+	 *
+	 * @return Disable_Errors_In_Plugins 
 	 */
 	public function __construct() {
 		// apply our custom error handler
@@ -32,9 +34,14 @@ class Disable_Errors_In_Plugins {
 	 *
 	 * @access public
 	 *
+	 * @param int $errno        The first parameter, errno, contains the level of the error raised.
+	 * @param string $errstr    The second parameter, errstr, contains the error message.
+	 * @param string $errfile   The third parameter is optional, errfile, which contains the filename that the error was raised in.
+	 * @param int $errline      The fourth parameter is optional, errline, which contains the line number the error was raised at.
+	 * @param array $errcontext The fifth parameter is optional, errcontext that contains an array of every variable that existed in the scope the error was triggered in. User error handler must not modify error context.
 	 * @return bool True if error is within /plugins, false otherwise.
 	 */
-	public function error_handler($errno, $errstr, $errfile, $errline, $errcontext) {
+	public function error_handler($errno, $errstr, $errfile, $errline, $errcontext = array()) {
 		// path to error file
 		$error_file = str_replace('\\', '/', $errfile);
 
